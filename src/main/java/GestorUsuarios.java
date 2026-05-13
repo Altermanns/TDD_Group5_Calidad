e6b51e9382dc81723079adfee9d0a9019b7b5adf
Isaac Trujillo <129634276+Altermanns@users.noreply.github.com>
2026-05-13 03:43:17 +0000

    Ciclo 1

--- src/main/java/GestorUsuarios.java
+++ src/main/java/GestorUsuarios.java
@@ -5,13 +5,13 @@ import java.util.regex.Pattern;
 public class GestorUsuarios {
     private Map<String, String> usuarios = new HashMap<>();
 
-   // public void agregarUsuario(String nombre, String correo) {
-   //     usuarios.put(nombre, correo);
-   // }
+   public void agregarUsuario(String nombre, String correo) {
+        usuarios.put(nombre, correo);
+    }
     
-   // public String getCorreo(String nombre) {
-     //   return usuarios.get(nombre);
-    //}
+    public String getCorreo(String nombre) {
+        return usuarios.get(nombre);
+    }
 } 
 
 
